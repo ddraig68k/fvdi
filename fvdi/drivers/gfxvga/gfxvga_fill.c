3f8792aa17ef86563054b5eb060cfc40aeeeea1a
Stephen Moody <stevemoody@fastmail.com>
2025-10-09 18:54:46 +0100

    Added hardware line drawing

--- fvdi/drivers/gfxvga/gfxvga_fill.c
+++ fvdi/drivers/gfxvga/gfxvga_fill.c
@@ -10,211 +10,19 @@
 #include "driver.h"
 #include "../bitplane/bitplane.h"
 
-#define FVDI_DEBUG 1
+/* #define FVDI_DEBUG 1 */
 #include "gfxvga.h"
 
 #define PIXEL		short
 #define PIXEL_SIZE	sizeof(PIXEL)
-#define PIXEL_32    long
-
-static int is_solid_pattern(const UWORD *pattern)
-{
-    for (int i = 0; i < 16; i++) {
-        DPRINTF(("pattern %d = %04X\n\r", i, pattern[i]));
-        if (pattern[i] != 0xFFFF)
-            return 0;
-    }
-    return 1;
-}
-
-/*
- * Make it as easy as possible for the C compiler.
- * The current code is written to produce reasonable results with Lattice C.
- * (long integers, optimize: [x xx] time)
- * - One function for each operation -> more free registers
- * - 'int' is the default type
- * - some compilers aren't very smart when it comes to *, / and %
- * - some compilers can't deal well with *var++ constructs
- */
-
-static void fill_replace(PIXEL *addr, PIXEL *addr_fast, int line_add, short *pattern, int x, int y, int w, int h, PIXEL foreground, PIXEL background)
-{
-    int i, j;
-    unsigned short pattern_word, mask;
-
-    (void) addr_fast;
-    i = y;
-    h = y + h;
-    x = 1 << (15 - (x & 0x000f));
-
-    /* Tell gcc that this cannot happen (already checked in c_fill_area() below) */
-    if (w <= 0 || h <= 0)
-        unreachable();
-    for(; i < h; i++) {
-        pattern_word = pattern[i & 0x000f];
-        switch (pattern_word) {
-        case 0xffff:
-            for(j = w - 1; j >= 0; j--) {
-                *addr = foreground;
-                addr++;
-            }
-            break;
-        default:
-            mask = x;
-            for(j = w - 1; j >= 0; j--) {
-                if (pattern_word & mask) {
-                    *addr = foreground;
-                    addr++;
-                } else {
-                    *addr = background;
-                    addr++;
-                }
-                if (!(mask >>= 1))
-                    mask = 0x8000;
-            }
-            break;
-        }
-        addr += line_add;
-    }
-}
-
-static void fill_transparent(PIXEL *addr, PIXEL *addr_fast, int line_add, short *pattern, int x, int y, int w, int h, PIXEL foreground, PIXEL background)
-{
-    int i, j;
-    unsigned short pattern_word, mask;
-
-    (void) addr_fast;
-    (void) background;
-    i = y;
-    h = y + h;
-    x = 1 << (15 - (x & 0x000f));
-
-    /* Tell gcc that this cannot happen (already checked in c_fill_area() below) */
-    if (w <= 0 || h <= 0)
-        unreachable();
-    for(; i < h; i++) {
-        pattern_word = pattern[i & 0x000f];
-        switch (pattern_word) {
-        case 0xffff:
-            for(j = w - 1; j >= 0; j--) {
-                *addr = foreground;
-                addr++;
-            }
-            break;
-        default:
-            mask = x;
-            for(j = w - 1; j >= 0; j--) {
-                if (pattern_word & mask) {
-                    *addr = foreground;
-                    addr++;
-                } else {
-                    addr++;
-                }
-                if (!(mask >>= 1))
-                    mask = 0x8000;
-            }
-            break;
-        }
-        addr += line_add;
-    }
-}
-
-static void fill_xor(PIXEL *addr, PIXEL *addr_fast, int line_add, short *pattern, int x, int y, int w, int h, PIXEL foreground, PIXEL background)
-{
-    int i, j;
-    unsigned short pattern_word, mask;
-    PIXEL v;
-
-    (void) addr_fast;
-    (void) foreground;
-    (void) background;
-    i = y;
-    h = y + h;
-    x = 1 << (15 - (x & 0x000f));
-
-    /* Tell gcc that this cannot happen (already checked in c_fill_area() below) */
-    if (w <= 0 || h <= 0)
-        unreachable();
-    for(; i < h; i++) {
-        pattern_word = pattern[i & 0x000f];
-        switch (pattern_word) {
-        case 0xffff:
-            for(j = w - 1; j >= 0; j--) {
-                v = ~*addr;
-                *addr = v;
-                addr++;
-            }
-            break;
-        default:
-            mask = x;
-            for(j = w - 1; j >= 0; j--) {
-                if (pattern_word & mask) {
-                    v = ~*addr;
-                    *addr = v;
-                    addr++;
-                } else {
-                    addr++;
-                }
-                if (!(mask >>= 1))
-                    mask = 0x8000;
-            }
-            break;
-        }
-        addr += line_add;
-    }
-}
-
-static void fill_revtransp(PIXEL *addr, PIXEL *addr_fast, int line_add, short *pattern, int x, int y, int w, int h, PIXEL foreground, PIXEL background)
-{
-    int i, j;
-    unsigned short pattern_word, mask;
-
-    (void) addr_fast;
-    (void) background;
-    i = y;
-    h = y + h;
-    x = 1 << (15 - (x & 0x000f));
-
-    /* Tell gcc that this cannot happen (already checked in c_fill_area() below) */
-    if (w <= 0 || h <= 0)
-        unreachable();
-    for(; i < h; i++) {
-        pattern_word = pattern[i & 0x000f];
-        switch (pattern_word) {
-        case 0x0000:
-            for(j = w - 1; j >= 0; j--) {
-                *addr = foreground;
-                addr++;
-            }
-            break;
-        default:
-            mask = x;
-            for(j = w - 1; j >= 0; j--) {
-                if (!(pattern_word & mask)) {
-                    *addr = foreground;
-                    addr++;
-                } else {
-                    addr++;
-                }
-                if (!(mask >>= 1))
-                    mask = 0x8000;
-            }
-            break;
-        }
-        addr += line_add;
-    }
-}
-
 
 long CDECL c_fill_area(Virtual *vwk, long x, long y, long w, long h,
                        short *pattern, long colour, long mode, long interior_style)
 {
-    Workstation *wk;
-    PIXEL *addr, *addr_fast;
+    //Workstation *wk;
     unsigned long foreground, background;
-    long line_add;
-    unsigned long pos;
     short *table;
+    (void) pattern;
 
     if (w <= 0 || h <= 0)
         return 1;
@@ -232,16 +40,17 @@ long CDECL c_fill_area(Virtual *vwk, long x, long y, long w, long h,
 
     c_get_colours(vwk, colour, &foreground, &background);
 
-    wk = vwk->real_address;
+    //wk = vwk->real_address;
 
     int16_t fill_type = (int16_t)((interior_style >> 16) & 0xFFFF);
     int16_t pattern_index = (int16_t)(interior_style & 0xFFFF);
-    int solid_pattern = (fill_type == 0) || (fill_type == 1) || ((fill_type == 2) && (pattern_index == 8)) 
-        || ((fill_type == 2) && (pattern_index == 4)) || is_solid_pattern(pattern);
+    // int solid_pattern = (fill_type == 0) || (fill_type == 1) || ((fill_type == 2) && (pattern_index == 8)) 
+    //     || ((fill_type == 2) && (pattern_index == 4));
 
     UWORD draw_mode = 0;
-    if (fill_type > 1)
-        draw_mode = DRAW_PATTERN(pattern_index + 2);
+    if (fill_type > 1) {
+        draw_mode = DRAW_PATTERN((pattern_index + 2));
+    }
 
     DPRINTF(("c_fill_area Interior style=%08lX fill_type=%d pattern_index=%d\n", interior_style, fill_type, pattern_index));
     
--- fvdi/drivers/gfxvga/gfxvga_line.c
+++ fvdi/drivers/gfxvga/gfxvga_line.c
@@ -16,242 +16,11 @@
 #define PIXEL_SIZE	sizeof(PIXEL)
 #define PIXEL_32    long
 
-/*
- * Make it as easy as possible for the C compiler.
- * The current code is written to produce reasonable results with Lattice C.
- * (long integers, optimize: [x xx] time)
- * - One function for each operation -> more free registers
- * - 'int' is the default type
- * - some compilers aren't very smart when it comes to *, / and %
- * - some compilers can't deal well with *var++ constructs
- */
-
-static void line_replace(PIXEL *addr, PIXEL *addr_fast, int count,
-                    int d, int incrE, int incrNE, int one_step, int both_step,
-                    PIXEL foreground, PIXEL background)
-{
-    *addr = foreground;
-    (void) addr_fast;
-    (void) background;
-
-    for(--count; count >= 0; count--) {
-        if (d < 0) {
-            d += incrE;
-            addr += one_step;
-        } else {
-            d += incrNE;
-            addr += both_step;
-        }
-        *addr = foreground;
-    }
-}
-
-static void line_replace_p(PIXEL *addr, PIXEL *addr_fast, long pattern, int count,
-                      int d, int incrE, int incrNE, int one_step, int both_step,
-                      PIXEL foreground, PIXEL background)
-{
-    unsigned short mask = 0x8000;
-
-    (void) addr_fast;
-    if (pattern & mask) {
-        *addr = foreground;
-    } else {
-        *addr = background;
-    }
-
-    for(--count; count >= 0; count--) {
-        if (d < 0) {
-            d += incrE;
-            addr += one_step;
-        } else {
-            d += incrNE;
-            addr += both_step;
-        }
-
-        if (!(mask >>= 1))
-            mask = 0x8000;
-
-        if (pattern & mask) {
-            *addr = foreground;
-        } else {
-            *addr = background;
-        }
-    }
-}
-
-static void line_transparent(PIXEL *addr, PIXEL *addr_fast, int count,
-                        int d, int incrE, int incrNE, int one_step, int both_step,
-                        PIXEL foreground, PIXEL background)
-{
-    *addr = foreground;
-    (void) addr_fast;
-    (void) background;
-
-    for(--count; count >= 0; count--) {
-        if (d < 0) {
-            d += incrE;
-            addr += one_step;
-        } else {
-            d += incrNE;
-            addr += both_step;
-        }
-        *addr = foreground;
-    }
-}
-
-static void line_transparent_p(PIXEL *addr, PIXEL *addr_fast, long pattern, int count,
-                          int d, int incrE, int incrNE, int one_step, int both_step,
-                          PIXEL foreground, PIXEL background)
-{
-    unsigned short mask = 0x8000;
-
-    (void) addr_fast;
-    (void) foreground;
-    (void) background;
-    if (pattern & mask) {
-        *addr = foreground;
-    }
-
-    for(--count; count >= 0; count--) {
-        if (d < 0) {
-            d += incrE;
-            addr += one_step;
-        } else {
-            d += incrNE;
-            addr += both_step;
-        }
-
-        if (!(mask >>= 1))
-            mask = 0x8000;
-
-        if (pattern & mask) {
-            *addr = foreground;
-        }
-    }
-}
-
-static void line_xor(PIXEL *addr, PIXEL *addr_fast, int count,
-                int d, int incrE, int incrNE, int one_step, int both_step,
-                PIXEL foreground, PIXEL background)
-{
-    int v;
-
-    (void) addr_fast;
-    (void) foreground;
-    (void) background;
-    v = ~*addr;
-    *addr = v;
-
-    for(--count; count >= 0; count--) {
-        if (d < 0) {
-            d += incrE;
-            addr += one_step;
-        } else {
-            d += incrNE;
-            addr += both_step;
-        }
-        v = ~*addr;
-        *addr = v;
-    }
-}
-
-static void line_xor_p(PIXEL *addr, PIXEL *addr_fast, long pattern, int count,
-                  int d, int incrE, int incrNE, int one_step, int both_step,
-                  PIXEL foreground, PIXEL background)
-{
-    int v;
-    unsigned short mask = 0x8000;
-
-    (void) addr_fast;
-    (void) foreground;
-    (void) background;
-    if (pattern & mask) {
-        v = ~*addr;
-        *addr = v;
-    }
-
-    for(--count; count >= 0; count--) {
-        if (d < 0) {
-            d += incrE;
-            addr += one_step;
-        } else {
-            d += incrNE;
-            addr += both_step;
-        }
-
-        if (!(mask >>= 1))
-            mask = 0x8000;
-
-        if (pattern & mask) {
-            v = ~*addr;
-            *addr = v;
-        }
-    }
-}
-
-static void line_revtransp(PIXEL *addr, PIXEL *addr_fast, int count,
-                      int d, int incrE, int incrNE, int one_step, int both_step,
-                      PIXEL foreground, PIXEL background)
-{
-    *addr = foreground;
-    (void) addr_fast;
-    (void) background;
-
-    for(--count; count >= 0; count--) {
-        if (d < 0) {
-            d += incrE;
-            addr += one_step;
-        } else {
-            d += incrNE;
-            addr += both_step;
-        }
-        *addr = foreground;
-    }
-}
-
-static void line_revtransp_p(PIXEL *addr, PIXEL *addr_fast, long pattern, int count,
-                        int d, int incrE, int incrNE, int one_step, int both_step,
-                        PIXEL foreground, PIXEL background)
-{
-    unsigned short mask = 0x8000;
-
-    (void) addr_fast;
-    (void) background;
-    if (!(pattern & mask)) {
-        *addr = foreground;
-    }
-
-    for(--count; count >= 0; count--) {
-        if (d < 0) {
-            d += incrE;
-            addr += one_step;
-        } else {
-            d += incrNE;
-            addr += both_step;
-        }
-
-        if (!(mask >>= 1))
-            mask = 0x8000;
-
-        if (!(pattern & mask)) {
-            *addr = foreground;
-        }
-    }
-}
-
 long CDECL c_line_draw(Virtual *vwk, long x1, long y1, long x2, long y2,
                        long pattern, long colour, long mode)
 {
     Workstation *wk;
-    PIXEL *addr, *addr_fast;
     unsigned long foreground, background;
-    int line_add;
-    long pos;
-    int x_step, y_step;
-    int dx, dy;
-    int one_step, both_step;
-    int d, count;
-    int incrE, incrNE;
 
     if ((long)vwk & 1) {
         return -1;          /* Don't know about anything yet */
@@ -264,84 +33,41 @@ long CDECL c_line_draw(Virtual *vwk, long x1, long y1, long x2, long y2,
 
     wk = vwk->real_address;
 
-    pos = (short)y1 * (long)wk->screen.wrap + x1 * 2;
-    addr = wk->screen.mfdb.address;
-    line_add = wk->screen.wrap >> 1;
-
-    x_step = 1;
-    y_step = line_add;
-
-    dx = x2 - x1;
-    if (dx < 0) {
-        dx = -dx;
-        x_step = -x_step;
-    }
-    dy = y2 - y1;
-    if (dy < 0) {
-        dy = -dy;
-        y_step = -y_step;
-    }
-
-    if (dx > dy) {
-        count = dx;
-        one_step = x_step;
-        incrE = 2 * dy;
-        incrNE = 2 * dy - 2 * dx;
-        d = 2 * dy - dx;
-    } else {
-        count = dy;
-        one_step = y_step;
-        incrE = 2 * dx;
-        incrNE = 2 * dx - 2 * dy;
-        d = 2 * dx - dy;
-    }
-    both_step = x_step + y_step;
-
-
-    addr += pos >> 1;
     if ((pattern & 0xffff) == 0xffff) {
         switch (mode) {
         case 1:             /* Replace */
             DPRINTF(("c_line_draw: mode=replace x1=%ld,y1=%ld,x2=%ld,y2=%ld color=%04X\n\r", x1, y1, x2, y2, (int)foreground));
-            // Check if horizontal or vertical line and solid line
-            if ((x1 == y2) || (x1 == y2))
-            {
-                // Use hardware acceleration
-                gfx_draw_line(0, x1, y1, x2, y2, foreground, background, 0);
-            } 
-            else
-                line_replace(addr, addr_fast, count, d, incrE, incrNE, one_step, both_step, foreground, background);
-            break;
+            gfx_draw_line(0, x1, y1, x2, y2, foreground, background, 0xFFFF);
         case 2:             /* Transparent */
             DPRINTF(("c_line_draw: mode=transparent x1=%ld,y1=%ld,x2=%ld,y2=%ld color=%04X\n\r", x1, y1, x2, y2, (int)foreground));
-            line_transparent(addr, addr_fast, count, d, incrE, incrNE, one_step, both_step, foreground, background);
+            /* Transparent line? Don't draw */
             break;
         case 3:             /* XOR */
             DPRINTF(("c_line_draw: mode=xor x1=%ld,y1=%ld,x2=%ld,y2=%ld color=%04X\n\r", x1, y1, x2, y2, (int)foreground));
-            line_xor(addr, addr_fast, count, d, incrE, incrNE, one_step, both_step, foreground, background);
+            gfx_draw_line(DRAW_MODE_XOR, x1, y1, x2, y2, foreground, background, 0xFFFF);
             break;
         case 4:             /* Reverse transparent */
             DPRINTF(("c_line_draw: mode=revtrans x1=%ld,y1=%ld,x2=%ld,y2=%ld color=%04X\n\r", x1, y1, x2, y2, (int)foreground));
-            line_revtransp(addr, addr_fast, count, d, incrE, incrNE, one_step, both_step, foreground, background);
+            /* Recerse Transparent line? Don't draw */
             break;
         }
     } else {
         switch (mode) {
         case 1:             /* Replace */
             DPRINTF(("c_line_draw: mode=pat_replace x1=%ld,y1=%ld,x2=%ld,y2=%ld pattern=%d color=%04X\n\r", x1, y1, x2, y2, (int)pattern, (int)foreground));
-            line_replace_p(addr, addr_fast, pattern, count, d, incrE, incrNE, one_step, both_step, foreground, background);
+            gfx_draw_line(DRAW_PATTERN(1), x1, y1, x2, y2, foreground, background, pattern);
             break;
         case 2:             /* Transparent */
             DPRINTF(("c_line_draw: mode=pat_trans x1=%ld,y1=%ld,x2=%ld,y2=%ld pattern=%d color=%04X\n\r", x1, y1, x2, y2, (int)pattern, (int)foreground));
-            line_transparent_p(addr, addr_fast, pattern, count, d, incrE, incrNE, one_step, both_step, foreground, background);
+            gfx_draw_line(DRAW_PATTERN(1) | DRAW_MODE_TRANS, x1, y1, x2, y2, foreground, background, pattern);
             break;
         case 3:             /* XOR */
             DPRINTF(("c_line_draw: mode=pat_xor x1=%ld,y1=%ld,x2=%ld,y2=%ld pattern=%d color=%04X\n\r", x1, y1, x2, y2, (int)pattern, (int)foreground));
-            line_xor_p(addr, addr_fast, pattern, count, d, incrE, incrNE, one_step, both_step, foreground, background);
+            gfx_draw_line(DRAW_PATTERN(1) | DRAW_MODE_XOR, x1, y1, x2, y2, foreground, background, pattern);
             break;
         case 4:             /* Reverse transparent */
             DPRINTF(("c_line_draw: mode=pat_revtrans x1=%ld,y1=%ld,x2=%ld,y2=%ld pattern=%d color=%04X\n\r", x1, y1, x2, y2, (int)pattern, (int)foreground));
-            line_revtransp_p(addr, addr_fast, pattern, count, d, incrE, incrNE, one_step, both_step, foreground, background);
+            gfx_draw_line(DRAW_PATTERN(1) | DRAW_MODE_REVTRANS, x1, y1, x2, y2, foreground, background, pattern);
             break;
         }
     }
